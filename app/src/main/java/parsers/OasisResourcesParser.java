3d7010b8368e7d3be060d287e188dcbfeb79b3c1
Shih-ChunHuang <schuang897@gmail.com>
2017-06-05 23:40:25 -0700

    started Parser

--- app/src/main/java/parsers/OasisResourcesParser.java
+++ app/src/main/java/parsers/OasisResourcesParser.java
@@ -4,5 +4,40 @@ package parsers;
  * Created by Shih-Chun on 2017-06-03.
  */
 
+
+import android.util.JsonReader;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.util.Set;
+
+import model.Amenity;
+import model.Oasis;
+
+/**
+ * A parser for the data returned by oasisdata.json
+ */
 public class OasisResourcesParser {
+
+
+    public static Set<Oasis> parseOasis(Amenity amenity, String jsonResponse) throws JSONException{
+
+        try{
+
+            JSONObject json = (JSONObject) new JSONTokener(jsonResponse).nextValue();
+
+
+
+        }catch (JSONException e){
+
+            e.printStackTrace();
+
+        }
+
+
+        return null;
+    }
+
 }
---
+++ app/src/main/java/test/OasisResourcesParserTest.java
@@ -0,0 +1,23 @@
+package test;
+
+
+import org.junit.Before;
+
+import java.io.InputStream;
+
+/**
+ * Created by Shih-Chun on 2017-06-05.
+ */
+
+public class OasisResourcesParserTest {
+
+
+    @Before
+    public void setUp() throws Exception{
+
+
+
+    }
+
+
+}
